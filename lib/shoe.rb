11ae50d8acc2200aea5ef03656ab0b1646290cf0
lyle dickie <lyle.e.dickie@gmail.com>
2019-09-18 20:52:41 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,22 @@
+require 'set'
+
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = Set.new
 
   def initialize(brand)
     @brand = brand
+    BRANDS.add(brand)
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  
 
 end
